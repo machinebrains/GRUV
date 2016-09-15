f35252af0cda4bff4e58b35fba3a4e3787b0a3f9
James Carroll <jamcar23@gmail.com>
2016-09-15 11:17:45 -0400

    Updated model size

--- config/nn_config.py
+++ config/nn_config.py
@@ -3,7 +3,7 @@ def get_neural_net_configuration():
 	nn_params['sampling_frequency'] = 44100
 	#Number of hidden dimensions.
 	#For best results, this should be >= freq_space_dims, but most consumer GPUs can't handle large sizes
-	nn_params['hidden_dimension_size'] = 2048
+	nn_params['hidden_dimension_size'] = 4096
 	#The weights filename for saving/loading trained models
 	nn_params['model_basename'] = './weights/MusicLibraryNPWeights'
 	#The model filename for the training data
